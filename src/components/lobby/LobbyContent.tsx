51a6f517f9684394ca0db57dd2ddf9b1e117921e
jadonamite <jadonamite@gmail.com>
2026-06-19 12:53:47 +0100

    fix(lobby): fix LobbyContent, profile

--- src/components/lobby/LobbyContent.tsx
+++ src/components/lobby/LobbyContent.tsx
@@ -376,12 +376,12 @@ export default function LobbyContent() {
                     </div>
                   ) : (
                     <>
-                      {openGames.slice((currentPage - 1) * ITEMS_PER_PAGE, currentPage * ITEMS_PER_PAGE).map((game, idx) => (
+                      {openGames.slice((currentPage - 1) * ITEMS_PER_PAGE, currentPage * ITEMS_PER_PAGE).map((game, index) => (
                     <motion.div
                       key={game.id}
                       initial={{ opacity: 0, y: 15 }}
                       animate={{ opacity: 1, y: 0 }}
-                      transition={{ delay: idx * 0.1 }}
+                      transition={{ delay: index * 0.1 }}
                       whileTap={{ scale: 0.985 }}
                     >
                       {/*
