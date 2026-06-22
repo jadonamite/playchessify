b6ce16f1a6b2207e75d3487b6fd0361121f03f48
jadonamite <jadonamite@gmail.com>
2026-06-22 20:28:39 +0100

    feat(app/layout): remove mobile chrome on game route.

--- src/components/lobby/HistoryContent.tsx
+++ src/components/lobby/HistoryContent.tsx
@@ -12,10 +12,6 @@ import { Queen, PieceView } from '@/components/ui/ChessModels'
 import { useBatchProfiles } from '@/hooks/useBatchProfiles'
 import ChessName from '@/components/ui/ChessName'
 
-/**
- * Scene
- * @returns {*}
- */
 function Scene() {
   return (
     <>
