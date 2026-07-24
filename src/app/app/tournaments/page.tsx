618a7ab2ef9680232201dcfa651e546c97c21246
jadonamite <jadonamite@gmail.com>
2026-07-24 21:42:49 +0100

    feat(app): add trailing comma in JSX return statements

--- src/app/app/tournaments/page.tsx
+++ src/app/app/tournaments/page.tsx
@@ -1,5 +1,5 @@
 import TournamentContent from '@/components/lobby/TournamentContent'
 
 export default function TournamentsPage() {
-  return <TournamentContent />
+  return <TournamentContent />,
 }
