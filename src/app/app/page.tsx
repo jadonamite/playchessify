d63ed6b7afa65d4486540672e5db91574386fe5b
jadonamite <jadonamite@gmail.com>
2026-07-25 19:19:21 +0100

    feat(app/page): refactor redirect logic

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,5 +1,9 @@
 import { redirect } from 'next/navigation'
 
-export default function AppPage() {
+const redirectToIntendedRoute = () => {
   redirect('/app/lobby')
+}
+
+export default function AppPage() {
+  redirectToIntendedRoute()
 }
