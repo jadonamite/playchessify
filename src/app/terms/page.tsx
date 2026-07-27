f115bf28cc2a53a680b152f5c59d7401a8f24649
jadonamite <jadonamite@gmail.com>
2026-07-27 21:35:21 +0100

    refactor(app): refactor page

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from 'next'
 import LegalPage from '@/components/legal/LegalPage'
+import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Terms of Service',
