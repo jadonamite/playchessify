68a7980c68621a64fe60b4a8cbd63d0c19bb4c24
jadonamite <jadonamite@gmail.com>
2026-07-27 20:44:03 +0100

    feat(src/components/game): add KingShatter component props

--- src/components/game/KingShatter.tsx
+++ src/components/game/KingShatter.tsx
@@ -37,6 +37,15 @@ interface Props {
   pieceSet: PieceSet
 }
 
+/**
+ * KingShatter
+ * @param {*} { square
+ * @param {*} color
+ * @param {*} orientation
+ * @param {*} boardTheme
+ * @param {*} pieceSet }: Props
+ * @returns {*}
+ */
 export default function KingShatter({ square, color, orientation, boardTheme, pieceSet }: Props) {
   const file = square.charCodeAt(0) - 97 // a-h → 0-7
   const rank = Number(square[1])         // 1-8
