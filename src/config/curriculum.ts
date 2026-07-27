c27ced3b81b1c22b0c78cb5865d45f0e78e83971
jadonamite <jadonamite@gmail.com>
2026-07-27 12:32:25 +0100

    chore: update curriculum

--- src/config/curriculum.ts
+++ src/config/curriculum.ts
@@ -65,6 +65,11 @@ export const CURRICULUM: Lesson[] = [
   },
 ]
 
+/**
+ * lessonById
+ * @param {*} id: string
+ * @returns {*}
+ */
 export function lessonById(id: string): Lesson | undefined {
   return CURRICULUM.find((l) => l.id === id)
 }
