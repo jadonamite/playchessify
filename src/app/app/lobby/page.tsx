3b7862704698f52db43c0143b1fb9a3fbd6b096f
jadonamite <jadonamite@gmail.com>
2026-07-27 19:36:41 +0100

    refactor(app): refactor page

--- src/app/app/lobby/page.tsx
+++ src/app/app/lobby/page.tsx
@@ -2,11 +2,11 @@
 
 import dynamic from 'next/dynamic'
 
+// Utility function to dynamically import components
+const importComponent = (path: string) => dynamic(() => import(path), { ssr: false })
+
 // Shell to prevent block-chain SDKs from leaking into the server build
-const LobbyContent = dynamic(
-  () => import('@/components/lobby/LobbyContent'),
-  { ssr: false }
-)
+const LobbyContent = importComponent('@/components/lobby/LobbyContent')
 
 export default function LobbyPage() {
   return <LobbyContent />
