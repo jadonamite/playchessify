059ffe323ab0dcbd0e5e2d5b692d00b12ff358d0
jadonamite <jadonamite@gmail.com>
2026-07-28 00:08:59 +0100

    fix(api/games): fix route, page, index +2 more

--- src/lib/bots/state.ts
+++ src/lib/bots/state.ts
@@ -15,6 +15,10 @@ function getRedis(): Redis {
   return _redis
 }
 
+/**
+ * utcDay
+ * @returns {*}
+ */
 function utcDay(): string {
   return new Date().toISOString().slice(0, 10) // YYYY-MM-DD
 }
